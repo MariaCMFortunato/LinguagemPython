edbdf550e4468b366efdd371e9fad3b11a1cae56
Maria <conceicao.martinho.estudo@gmail.com>
2024-02-08 19:52:40 +0000

    Alterações

--- projetoConsumosVeiculos/consumoVeiculos.py
+++ projetoConsumosVeiculos/consumoVeiculos.py
@@ -1,6 +1,8 @@
 import pandas
 import sqlite3
+import os
 from time import sleep
+
 
 # Classes da aplicação Consumos de Veículos:
 
@@ -74,6 +76,9 @@ class ImportaDados:
     #     pass
 
     def ficheiroAImportar():
+        # recebe a indicação do ficheiro a importar e do caminho absoluto concatena a informação
+        # depois de mudar a barra e devolve o caminho completo do ficheiro para importação, sem
+        # extensão do mesmo
         FICHEIRO = input(
             "\033[36mIndique o nome do ficheiro a importar: \033[m")
         CAMINHO = input(
@@ -83,6 +88,9 @@ class ImportaDados:
         return CaminhoCompleto
 
     def colunasAImportar(consumos):
+        # tendo em conta que pode não ser necessário importar todas as colunas do ficheiro
+        # pede-se ao utilizador que indique as colunas a importar, parando o ciclo com uma
+        # string vazia
         i = 0
         colunas = []
         print("")
@@ -92,85 +100,138 @@ class ImportaDados:
                 colunas.append(coluna)
             else:
                 break
-
+        # criar o subconjunto das colunas a importar
         consumoSubset = consumos[colunas]
         # ["data", "valor", "preço litro", "kms totais"]
+        # retorna as primeiras 40 linhas
         consumoSubset.head(40)
+        # retorna o subconjunto de dados criado
         return consumoSubset
 
     def importaCSV():
+        # instância que a informação do ficheiro e do caminho do ficheiro a importar
         caminho = ImportaDados.ficheiroAImportar()
-        consumosf2 = pandas.read_csv(f"{caminho}.csv", sep=";")
-        consumosf2.head(40)
-        consumosf3 = ImportaDados.colunasAImportar(consumosf2)
-        df_dados = pandas.DataFrame(consumosf3)
-        ImportaDados.imprimirDados(df_dados)
+        # instância que aramzena os dados do ficheiro importado
+        consumosf = pandas.read_csv(f"{caminho}.csv", sep=";")
+        consumosf2 = ImportaDados.colunasAImportar(consumosf)
+        df_dados = pandas.DataFrame(consumosf2)
+        return df_dados
+        # ImportaDados.imprimirDados(df_dados)
         # consumosf2.rename(columns={"Data":"data", "Total Pago":"valor", "Valor p/ Litro":"preço litro", "Litros":"litros", "Km Antes / Após do Abastecimento":"kms totais","Km Percorridos":"km percorridos","Media de Consumo por 100Km":"Consumo aos 100 kms" })
 
     def importaEXCEL():
         caminho = ImportaDados.ficheiroAImportar()
         consumosf = pandas.read_excel(f"{caminho}.xlsx")
-        consumosf.head(40)
-        consumosf3 = ImportaDados.colunasAImportar(consumosf)
-        df_dados = pandas.DataFrame(consumosf3)
-        ImportaDados.imprimirDados(df_dados)
+        consumosf2 = ImportaDados.colunasAImportar(consumosf)
+        df_dados = pandas.DataFrame(consumosf2)
+        return df_dados
+        # ImportaDados.imprimirDados(df_dados)
         # consumosf.rename(columns={"Data":"data", "Total Pago":"valor", "Valor p/ Litro":"preço litro", "Litros":"litros", "Km Antes / Após do Abastecimento":"kms totais","Km Percorridos":"km percorridos","Media de Consumo por 100Km":"Consumo aos 100 kms" })
 
     def imprimirDados(df_dados):
+        # imprime os dado no ecrã
         print("")
-        print(linha())
+        print(Menu.linha())
         print(df_dados)
-        print(linha())
-        sleep(3)
+        print(Menu.linha())
+        os.system("pause")
+        # sleep(3)
 
 
-def linha(tamanho=50):
-    return "\033[33m-\033[m" * tamanho
+class ExportaDados:
+    def dadosAExportar():
+        BASEDADOS = input(
+            "\033[36mIndique o nome da Base de Dados: \033[m")
+        CAMINHO = input(
+            "\033[36mIndique o caminho do ficheiro: \033[m")
+        CAMINHO.replace('\\', '/')
+        bDados = CAMINHO+"/"+BASEDADOS
+        return bDados
+
+    def menuBD():
+        escolhas = [
+            "Preciso de importar um fciheiro CSV",
+            "Preciso de importar um fciheiro EXCEL",
+            "Já importei os dados a exportar",
+            "Menu Prinicipal"
+        ]
+
+        while True:
+            os.system("cls")
+            Menu.cabecalho("\033[33mDADOS A EXPORTAR\033[m")
+            escolha = Menu.menu(escolhas)
+            if escolha == 1:
+                Menu.cabecalho("\033[33mIMPORTAR FICHEIRO CSV\033[m")
+                df_dados = ImportaDados.importaCSV()
+            elif escolha == 2:
+                Menu.cabecalho("\033[33mIMPORTAR FICHEIRO EXCEL\033[m")
+                df_dados = ImportaDados.importaEXCEL()
+            elif escolha == 3:
+                Menu.cabecalho("\033[33mEXPORTAR DADOS PARA TABELA SQL\033[m")
+                ImportaDados.imprimirDados(df_dados)
+                ExportaDados.criarBD(df_dados)
+            elif escolha == 4:
+                Menu.cabecalho("\033[33mO PROGRAMA VAI TERMINAR!\033[m")
+                break
+            else:
+                Menu.cabecalho("\033[31mERRO! Escolha uma opção válida!\033[m")
+                sleep(2)
+
+    def criarBD(df_dados):
+        baseDados = ExportaDados.dadosAExportar()
+        conexao = sqlite3.connect(f"{baseDados}.db")
+        df_dados.to_sql(name="consumos", con=conexao,
+                        if_exists="replace", index=True, index_label="indice")
+
+        conexao.close
 
 
-def cabecalho(texto):
-    print(linha())
-    print(texto.center(50))
-    print(linha())
+class Menu:
+    def linha(tamanho=50):
+        return "\033[33m-\033[m" * tamanho
 
+    def cabecalho(texto):
+        print(Menu.linha())
+        print(texto.center(50))
+        print(Menu.linha())
 
-def menu(opcoes):
-    cabecalho("\033[33mMENU PRINCIPAL\033[m")
-    contador = 1
-    for item in opcoes:
-        print(f"\033[33m{contador}\033[m - \033[34m{item}\033[m")
-        contador += 1
-    print(linha())
-    opcao = int(input("\033[32mDigite a sua opção: \033[m"))
-    return opcao
+    def menu(opcoes):
+        contador = 1
+        for item in opcoes:
+            print(f"\033[33m{contador}\033[m - \033[34m{item}\033[m")
+            contador += 1
+        print(Menu.linha())
+        opcao = int(input("\033[32mDigite a sua opção: \033[m"))
+        return opcao
 
-
-opcoes = [
-    "Importar ficheiro CSV com dados da viatura",
-    "Importar ficheiro EXCEL com dados da viatura",
-    "Exportar dados para SQLite3",
-    "Calculo do consumo de um veículo aos 100 kms",
-    "Sair do programa"
-]
+    opcoes = [
+        "Importar ficheiro CSV com dados da viatura",
+        "Importar ficheiro EXCEL com dados da viatura",
+        "Exportar dados para SQLite3",
+        "Calculo do consumo de um veículo aos 100 kms",
+        "Sair do programa"
+    ]
 
 
 if __name__ == '__main__':
-
     while True:
-        escolha = menu(opcoes)
+        os.system("cls")
+        Menu.cabecalho("\033[33mMENU PRINCIPAL\033[m")
+        escolha = Menu.menu(Menu.opcoes)
         if escolha == 1:
-            cabecalho("\033[33mIMPORTAR FICHEIRO CSV\033[m")
+            Menu.cabecalho("\033[33mIMPORTAR FICHEIRO CSV\033[m")
             ImportaDados.importaCSV()
         elif escolha == 2:
-            cabecalho("\033[33mIMPORTAR FICHEIRO EXCEL\033[m")
+            Menu.cabecalho("\033[33mIMPORTAR FICHEIRO EXCEL\033[m")
             ImportaDados.importaEXCEL()
         elif escolha == 3:
-            cabecalho("\033[33mEXPORTAR DADOS PARA TABELA SQL\033[m")
+            Menu.cabecalho("\033[33mEXPORTAR DADOS PARA TABELA SQL\033[m")
+            ExportaDados.menuBD()
         elif escolha == 4:
-            cabecalho("\033[33mCALCULAR CONSUMO DE UM VEÍCULO\033[m")
+            Menu.cabecalho("\033[33mCALCULAR CONSUMO DE UM VEÍCULO\033[m")
         elif escolha == 5:
-            cabecalho("\033[33mO PROGRAMA VAI TERMINAR!\033[m")
+            Menu.cabecalho("\033[33mO PROGRAMA VAI TERMINAR!\033[m")
             break
         else:
-            cabecalho("\033[31mERRO! Escolha uma opção válida!\033[m")
+            Menu.cabecalho("\033[31mERRO! Escolha uma opção válida!\033[m")
             sleep(2)
